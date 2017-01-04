f9a31b4d62bb22588b6eab88c519ec45403b77be
Ilia Cimpoes <ichimpoesh@gmail.com>
2017-01-05 00:13:50 +0200

    Added listOfN, many, map

--- src/main/scala/chapter_9/Parsers.scala
+++ src/main/scala/chapter_9/Parsers.scala
@@ -1,5 +1,7 @@
 package chapter_9
 
+import chapter_8.{Gen, Prop}
+
 trait Parsers[ParseError, Parser[+ _]] {
   self =>
 
@@ -17,10 +19,34 @@ trait Parsers[ParseError, Parser[+ _]] {
 
   implicit def asCharParser[A](a: A)(implicit f: A => Parser[Char]): ParserOps[Char] = ParserOps(f(a))
 
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = ???
+
+  def many[A](p: Parser[A]): Parser[List[A]] = ???
+
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B] = ???
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
+    def many: Parser[List[A]] = self.many(p)
+
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+  }
+
+
+  object Laws {
+
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      in.forAll(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
+  }
+
+  object impl {
+    val numA: Parser[Int] = char('a').many.map(_.size)
   }
 
 }
--- src/test/scala/chapter_9/ParsersSpec.scala
+++ src/test/scala/chapter_9/ParsersSpec.scala
@@ -24,4 +24,17 @@ class ParsersSpec extends Properties("Parsers") {
 //    run(s1 | s2)(s2) == Right(s2)
 //  }
 
+//  property("listOfN") = forAll { (s1: String, s2: String) =>
+//    run(listOfN(3, s1 | s2))(s1 * 3) == Right(s1 * 3) &&
+//      run(listOfN(3, s1 | s2))(s1 * 2 + s2) == Right(s1 * 2 + s2) &&
+//      run(listOfN(3, s1 | s2))(s1 + s2 * 2) == Right(s1 + s2 * 2) &&
+//      run(listOfN(3, s1 | s2))(s2 * 3) == Right(s2 * 3) &&
+//      run(listOfN(3, s1 | s2))(s2 * 2 + s1) == Right(s2 * 2 + s1) &&
+//      run(listOfN(3, s1 | s2))(s2 + s1 * 2) == Right(s2 + s1 * 2)
+//  }
+
+//  property("numA") = forAll { (s: String) =>
+//    run(impl.numA)(s) == Right(s.count(_ == 'a'))
+//  }
+
 }

5407389c415e336d548f24ff7bb335f15fe94c4a
Ilia Cimpoes <ichimpoesh@gmail.com>
2017-01-21 22:25:22 +0200

    Started tests for persers

--- src/test/scala/chapter_9/ParsersSpec.scala
+++ src/test/scala/chapter_9/ParsersSpec.scala
@@ -1,40 +1,51 @@
 package chapter_9
 
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
 import org.scalacheck.Prop.forAll
+import org.scalacheck.Arbitrary._
 
 
 class ParsersSpec extends Properties("Parsers") {
 
-//  val parsers = new Parsers[String, Parser] {}
+  val p = SimpleParser.parser
 
-//  import parsers._
+  import p._
 
-// Not Implemented
-//  property("char") = forAll { (c: Char) =>
-//    run(c)(c.toString) == Right(c)
+  val nonEmpty: Gen[String] = arbitrary[String].suchThat(_.nonEmpty)
+  val byReg: Gen[String] = arbitrary[String].suchThat(s => "[a-zA-Z0-9_ ']+".r.findFirstMatchIn(s).nonEmpty)
+
+  property("char") = forAll { (c: Char) =>
+    run(c)(c.toString) == Right(c)
+  }
+
+  property("string()") = forAll { (s: String) =>
+      p.run(s)(s) == Right(s)
+  }
+
+  property("listOfN") = forAll(nonEmpty, nonEmpty) { (s1: String, s2: String) =>
+    run(listOfN(3, s1 | s2))(s1 * 3) == Right(List.fill(3)(s1)) &&
+      run(listOfN(3, s1 | s2))(s1 * 2 + s2) == Right(s1 :: s1 :: s2 :: Nil) &&
+      run(listOfN(3, s1 | s2))(s1 + s2 * 2) == Right(s1 :: s2 :: s2 :: Nil) &&
+      run(listOfN(3, s1 | s2))(s2 * 3) == Right(List.fill(3)(s2)) &&
+      run(listOfN(3, s1 | s2))(s2 * 2 + s1) == Right(s2 :: s2 :: s1 :: Nil) &&
+      run(listOfN(3, s1 | s2))(s2 + s1 * 2) == Right(s2 :: s1 :: s1 :: Nil)
+  }
+
+  property("or") = forAll(nonEmpty, nonEmpty) { (s1: String, s2: String) =>
+    p.run(s1 | s2)(s1) == Right(s1) &&
+    p.run(s1 | s2)(s2) == Right(s2)
+  }
+
+//  property("numA") = forAll(byReg) { (s: String) =>
+//    p.run(impl.numA)(s) == Right(s.count(_ == 'a'))
+//  }
+//
+//  property("string") = forAll(byReg) { (s: String) =>
+//    p.run(string)(s""""$s"""") == Right(s)
 //  }
 
-//  property("string") = forAll { (s: String) =>
-//    run(s)(s) == Right(s)
-//  }
-
-//  property("or") = forAll { (s1: String, s2: String) =>
-//    run(s1 | s2)(s1) == Right(s1) &&
-//    run(s1 | s2)(s2) == Right(s2)
-//  }
-
-//  property("listOfN") = forAll { (s1: String, s2: String) =>
-//    run(listOfN(3, s1 | s2))(s1 * 3) == Right(s1 * 3) &&
-//      run(listOfN(3, s1 | s2))(s1 * 2 + s2) == Right(s1 * 2 + s2) &&
-//      run(listOfN(3, s1 | s2))(s1 + s2 * 2) == Right(s1 + s2 * 2) &&
-//      run(listOfN(3, s1 | s2))(s2 * 3) == Right(s2 * 3) &&
-//      run(listOfN(3, s1 | s2))(s2 * 2 + s1) == Right(s2 * 2 + s1) &&
-//      run(listOfN(3, s1 | s2))(s2 + s1 * 2) == Right(s2 + s1 * 2)
-//  }
-
-//  property("numA") = forAll { (s: String) =>
-//    run(impl.numA)(s) == Right(s.count(_ == 'a'))
-//  }
+  property("succeed") = forAll { (i: Int) =>
+    p.run(succeed(i))("") == Right(i)
+  }
 
 }

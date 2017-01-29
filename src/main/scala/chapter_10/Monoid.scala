ea1fc972e280680acd125a3290cc3bbc8926e9a8
Ilia Cimpoes <ichimpoesh@gmail.com>
2017-01-30 00:42:45 +0200

    Starting Monoid

---
+++ src/main/scala/chapter_10/Monoid.scala
@@ -0,0 +1,76 @@
+package chapter_10
+
+import chapter_8.{Gen, Prop}
+
+trait Monoid[A] {
+
+  def op(a1: A, a2: A): A
+
+  def zero: A
+
+}
+
+object Monoid {
+  val stringMonoid = new Monoid[String] {
+    override def op(a1: String, a2: String): String = a1 + a2
+
+    override def zero: String = ""
+  }
+
+  def listMonoid[A] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
+    override def zero: List[A] = Nil
+  }
+
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+
+    override def zero: Int = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+
+    override def zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override def zero: Boolean = true
+  }
+
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+
+    override def zero: Option[A] = None
+  }
+
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[(A) => A] {
+    override def op(a1: (A) => A, a2: (A) => A): (A) => A = a1 compose a2
+
+    override def zero = identity
+  }
+
+  def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = {
+    val assoc = for {
+      x <- gen
+      y <- gen
+      z <- gen
+    } yield m.op(x, m.op(y, z)) == m.op(m.op(x, y), z)
+
+    assoc.forAll(identity)
+
+    gen.forAll(a => m.op(a, m.zero) == a)
+  }
+
+
+}

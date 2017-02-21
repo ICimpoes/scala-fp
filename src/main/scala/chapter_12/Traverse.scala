6f19ae952dff93fdfee97a61fe6bc805379def82
Ilia Cimpoes <ichimpoesh@gmail.com>
2017-02-21 23:15:31 +0200

    Implemented Tree traverse

--- src/main/scala/chapter_12/Traverse.scala
+++ src/main/scala/chapter_12/Traverse.scala
@@ -1,6 +1,7 @@
 package chapter_12
 
 import chapter_11.Functor
+import chapter_3.{Branch, Leaf, Tree}
 
 trait Traverse[F[_]] extends Functor[F] {
 
@@ -27,6 +28,13 @@ object Traverse {
 
     override def traverse[G[_] : Applicative, A, B](fa: Option[A])(f: (A) => G[B]): G[Option[B]] =
       fa.fold(Applicative[G]unit(None: Option[B]))(a => Applicative[G].map(f(a))(Some(_)))
+  }
+
+  val treeTraverse = new Traverse[Tree] {
+    override def map[A, B](fa: Tree[A])(f: (A) => B): Tree[B] = fa.map(f)
+
+    override def traverse[G[_] : Applicative, A, B](fa: Tree[A])(f: (A) => G[B]): G[Tree[B]] =
+      fa.fold(a => Applicative[G].map(f(a))(Leaf(_): Tree[B]))((gb1, gb2) => Applicative[G].map2(gb1, gb2)((t1, t2) => Branch(t1, t2): Tree[B]))
   }
 
 }
--- src/test/scala/chapter_12/TraverseSpec.scala
+++ src/test/scala/chapter_12/TraverseSpec.scala
@@ -1,17 +1,30 @@
 package chapter_12
 
+import chapter_3.{Branch, Leaf, Tree}
 import org.scalatest.{FlatSpec, Matchers}
 
 
 class TraverseSpec extends FlatSpec with Matchers {
 
-  "Traverse.listTraverse" should "traverse List[Option] to Option[List]" in {
+  import chapter_12.Applicative.optionApplicative
 
-    import chapter_12.Applicative.optionApplicative
+  "Traverse.listTraverse" should "traverse List[Option] to Option[List]" in {
 
     Traverse.listTraverse.traverse[Option, Int, String](List(1, 2, 3))(i => Some(i.toString)) shouldBe Some(List("1", "2", "3"))
     Traverse.listTraverse.traverse[Option, Int, String](List(1, 2, 3))(i => if (i % 2 == 0) Some(i.toString) else None) shouldBe None
     Traverse.listTraverse.traverse[Option, Int, String](List())(i => if (i % 2 == 0) Some(i.toString) else None) shouldBe Some(Nil)
+
+  }
+  "Traverse.treeTraverse" should "traverse List[Option] to Option[List]" in {
+
+    import chapter_12.Applicative.optionApplicative
+
+    val tree: Tree[Int] = Branch(Branch(Leaf(2), Leaf(3)), Leaf(1))
+
+    Traverse.treeTraverse.traverse[Option, Int, String](tree)(i => Some(i.toString)) shouldBe Some(tree.map(_.toString))
+    Traverse.treeTraverse.traverse[Option, Int, String](tree)(i => if (i % 2 == 0) Some(i.toString) else None) shouldBe None
+    Traverse.treeTraverse.traverse[Option, Int, String](Leaf(1))(i => if (i % 2 == 0) Some(i.toString) else None) shouldBe None
+    Traverse.treeTraverse.traverse[Option, Int, String](Leaf(1))(i => Some(i.toString)) shouldBe Some(Leaf("1"))
 
   }
 

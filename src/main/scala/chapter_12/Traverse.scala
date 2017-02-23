6c7f799dd9e26d22256a4e7b2fc421ece13983a5
Ilia Cimpoes <ichimpoesh@gmail.com>
2017-02-24 00:18:00 +0200

    Implemented 'toList' and 'zipWithIndex' for 'Traverse'. Also added implementetion using 'Traverse[State]'

--- src/main/scala/chapter_12/Traverse.scala
+++ src/main/scala/chapter_12/Traverse.scala
@@ -2,23 +2,59 @@ package chapter_12
 
 import chapter_10.Monoid.Const
 import chapter_10.{Foldable, Monoid}
-import chapter_11.{Functor, Id}
+import chapter_11.{Functor, Id, Monad}
 import chapter_3.{Branch, Leaf, Tree}
+import chapter_6.State
+import chapter_6.State._
 
 trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
 
   def traverse[G[_] : Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]] =
     sequence(map(fa)(f))
 
+
   def sequence[G[_] : Applicative, A](fga: F[G[A]]): G[F[A]] =
     traverse(fga)(identity)
 
-  def map[A,B](fa: F[A])(f: A => B): F[B] =
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
     traverse(fa)(a => Id(f(a))).a
 
   override def foldMap[A, M](as: F[A])(f: A => M)(mb: Monoid[M]): M =
     traverse[({type f[x] = Const[M, x]})#f, A, Nothing](
       as)(f)(Monoid.monoidApplicative(mb))
+
+  def traverseS[S, A, B](fa: F[A])(f: A => State[S, B]): State[S, F[B]] =
+    traverse[({type f[x] = State[S, x]})#f, A, B](fa)(f)(Monad.stateMonad)
+
+  def zipWithIndex[A](ta: F[A]): F[(A, Int)] =
+    traverseS(ta)((a: A) => for {
+      i <- get[Int]
+      _ <- set(i + 1)
+    } yield (a, i)).run(0)._1
+
+  override def toList[A](fa: F[A]): List[A] =
+    traverseS(fa) { (a: A) =>
+      for {
+        l <- get[List[A]]
+        _ <- set(a :: l)
+      } yield ()
+    }.run(Nil)._2.reverse
+
+  def toList_[A](fa: F[A]): List[A] =
+    mapAccum[A, List[A], Unit](fa)((a, l) => ((), a :: l))(Nil)._2.reverse
+
+  def zipWithIndex_[A](ta: F[A]): F[(A, Int)] =
+    mapAccum[A, Int, (A, Int)](ta)((a, i: Int) => (a -> i) -> (i + 1))(0)._1
+
+  def mapAccum[A, B, R](fa: F[A])(f: (A, B) => (R, B))(z: B): (F[R], B) =
+    traverseS(fa) { (a: A) =>
+      for {
+        b <- get[B]
+        (r, b1) = f(a, b)
+        _ <- set(b1)
+      } yield r
+    }.run(z)
+
 }
 
 object Traverse {
@@ -35,7 +71,7 @@ object Traverse {
     override def map[A, B](fa: Option[A])(f: (A) => B) = fa.map(f)
 
     override def traverse[G[_] : Applicative, A, B](fa: Option[A])(f: (A) => G[B]): G[Option[B]] =
-      fa.fold(Applicative[G]unit(None: Option[B]))(a => Applicative[G].map(f(a))(Some(_)))
+      fa.fold(Applicative[G] unit (None: Option[B]))(a => Applicative[G].map(f(a))(Some(_)))
   }
 
   val treeTraverse = new Traverse[Tree] {
--- src/test/scala/chapter_12/TraverseSpec.scala
+++ src/test/scala/chapter_12/TraverseSpec.scala
@@ -6,6 +6,8 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TraverseSpec extends FlatSpec with Matchers {
 
+  val tree: Tree[Int] = Branch(Branch(Leaf(2), Leaf(3)), Leaf(1))
+
   import chapter_12.Applicative.optionApplicative
 
   "Traverse.listTraverse" should "traverse List[Option] to Option[List]" in {
@@ -19,8 +21,6 @@ class TraverseSpec extends FlatSpec with Matchers {
 
     import chapter_12.Applicative.optionApplicative
 
-    val tree: Tree[Int] = Branch(Branch(Leaf(2), Leaf(3)), Leaf(1))
-
     Traverse.treeTraverse.traverse[Option, Int, String](tree)(i => Some(i.toString)) shouldBe Some(tree.map(_.toString))
     Traverse.treeTraverse.traverse[Option, Int, String](tree)(i => if (i % 2 == 0) Some(i.toString) else None) shouldBe None
     Traverse.treeTraverse.traverse[Option, Int, String](Leaf(1))(i => if (i % 2 == 0) Some(i.toString) else None) shouldBe None
@@ -31,5 +31,22 @@ class TraverseSpec extends FlatSpec with Matchers {
     Traverse.optionTraverse.map(Some(1))(_.toString) shouldBe Some("1")
     Traverse.optionTraverse.map(None)(identity) shouldBe None
   }
+  "Traverse.listTraverse" should "correctly zip list with index" in {
+    Traverse.listTraverse.zipWithIndex(List(1, 2, 3)) shouldBe List(1, 2, 3).zipWithIndex
+    Traverse.listTraverse.zipWithIndex(Nil) shouldBe Nil
 
+    Traverse.listTraverse.zipWithIndex_(List(1, 2, 3)) shouldBe List(1, 2, 3).zipWithIndex
+    Traverse.listTraverse.zipWithIndex_(Nil) shouldBe Nil
+  }
+  "Traverse.toList" should "correctly convert a traversable to list" in {
+    Traverse.optionTraverse.toList(Some(1)) shouldBe List(1)
+    Traverse.optionTraverse.toList(None) shouldBe Nil
+    Traverse.treeTraverse.toList(tree) shouldBe List(2, 3, 1)
+    Traverse.treeTraverse.toList(Leaf(1)) shouldBe List(1)
+
+    Traverse.optionTraverse.toList_(Some(1)) shouldBe List(1)
+    Traverse.optionTraverse.toList_(None) shouldBe Nil
+    Traverse.treeTraverse.toList_(tree) shouldBe List(2, 3, 1)
+    Traverse.treeTraverse.toList_(Leaf(1)) shouldBe List(1)
+  }
 }

577a51b0e9352922724ad91307a3d525bacb171d
Ilia Cimpoes <ichimpoesh@gmail.com>
2017-03-26 00:55:26 +0200

    STArray defined

--- src/main/scala/chapter_14/ST.scala
+++ src/main/scala/chapter_14/ST.scala
@@ -1,5 +1,7 @@
 package chapter_14
 
+import scala.reflect.ClassTag
+
 sealed trait ST[S, A] {
   self =>
   protected def run(s: S): (A, S)
@@ -51,4 +53,33 @@ object STRef {
   def apply[S, A](a: A): ST[S, STRef[S, A]] = ST(new STRef[S, A] {
     var cell: A = a
   })
+}
+
+
+sealed abstract class STArray[S, A: ClassTag] {
+  protected def value: Array[A]
+
+  def size: ST[S, Int] = ST(value.length)
+
+  def write(i: Int, a: A): ST[S, Unit] = (s: S) => {
+    value(i) = a
+    ((), s)
+  }
+
+  def read(i: Int): ST[S, A] = ST(value(i))
+
+  def freeze: ST[S, List[A]] = ST(value.toList)
+
+  def fill(xs: Map[Int, A]): ST[S, Unit] = xs.foldLeft(ST[S, Unit]()) {
+    case (s, (i, a)) =>
+      s.flatMap(_ => write(i, a))
+  }
+}
+
+object STArray {
+
+  def apply[S, A: ClassTag](sz: Int, v: A): ST[S, STArray[S, A]] = ST(new STArray[S, A] {
+    lazy val value: Array[A] = Array.fill(sz)(v)
+  })
+
 }
--- src/main/scala/chapter_14/STApp.scala
+++ src/main/scala/chapter_14/STApp.scala
@@ -8,13 +8,31 @@ object STApp extends App {
       r2 <- STRef(2)
       x <- r1.read
       y <- r2.read
-      _ <- r1.write(y+1)
-      _ <- r2.write(x+1)
+      _ <- r1.write(y + 1)
+      _ <- r2.write(x + 1)
       a <- r1.read
       b <- r2.read
-    } yield (a,b)
+    } yield (a, b)
   }
 
   println(ST.runST(p))
+
+
+
+  ST.runST(new RunnableST[Unit] {
+    override def apply[S]: ST[S, Unit] = for {
+      a <- STArray(10, "1")
+      a1 <- a.fill(Map(1 -> "2", 2 -> "3", 3 -> "4", 4 -> "5"))
+      x0 <- a.read(0)
+      x1 <- a.read(1)
+      x2 <- a.read(2)
+      x3 <- a.read(3)
+      x4 <- a.read(4)
+      x5 <- a.read(5)
+      x6 <- a.read(6)
+    } yield println(s"$x0 $x1 $x2 $x3 $x4 $x5 $x6")
+  })
+
+
 
 }

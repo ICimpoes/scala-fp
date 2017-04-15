e5b07793f897384609bc5b41d823da548bc845f8
Ilia Cimpoes <ichimpoesh@gmail.com>
2017-04-16 00:37:52 +0300

    'runLog' for generic Monad

--- src/main/scala/chapter_15/extensible/Process.scala
+++ src/main/scala/chapter_15/extensible/Process.scala
@@ -1,9 +1,22 @@
 package chapter_15.extensible
 
+import chapter_11.Monad
+import chapter_11.Monad.Ops._
 import chapter_13.IO
 import chapter_15.extensible.Process._
 
 trait Process[F[_], O] {
+
+  def runLog(implicit F: MonadCatch[F]): F[IndexedSeq[O]] = {
+    def go(cur: Process[F, O], acc: IndexedSeq[O]): F[IndexedSeq[O]] =
+      cur match {
+        case Emit(h, t) => go(t, acc :+ h)
+        case Halt(End) => F.unit(acc)
+        case Halt(err) => F.fail(err)
+        case Await(req, recv) => F.attempt(req).flatMap(x => go(Try(recv(x)), acc))
+      }
+    go(this, IndexedSeq())
+  }
 
   def onHalt(f: Throwable => Process[F, O]): Process[F, O] = this match {
     case Halt(e) => Try(f(e))
@@ -28,6 +41,12 @@ trait Process[F[_], O] {
 }
 
 object Process {
+
+  trait MonadCatch[F[_]] extends Monad[F] {
+    def attempt[A](a: F[A]): F[Either[Throwable, A]]
+
+    def fail[A](t: Throwable): F[A]
+  }
 
   def Try[F[_], O](p: => Process[F, O]): Process[F, O] =
     try p
@@ -68,4 +87,20 @@ object Process {
     try go(src, IndexedSeq())
     finally ()
   }
+
+  import java.io.{BufferedReader, FileReader}
+
+  def p(fileName: String): Process[IO, String] =
+    await(IO(new BufferedReader(new FileReader(fileName)))) {
+      case Right(b) =>
+        lazy val next: Process[IO, String] = await(IO(b.readLine)) {
+          case Left(e) => await(IO(b.close))(_ => Halt(e))
+          case Right(line) =>
+            if (line eq null) Halt(End)
+            else Emit(line, next)
+        }
+        next
+      case Left(e) => Halt(e)
+    }
+
 }
